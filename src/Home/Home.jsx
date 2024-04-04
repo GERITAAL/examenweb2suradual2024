6ca74742dd39a2822ee90307dfcb8c00df1d2205
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-04 16:44:00 -0500

    examen correjido

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,11 @@
 import "./Home.css"
+import basedatos from"../utils/basedatos.json"
 
 export function Home(){
 
     //logica para leer la base de datos
 
-
+    
     return(
 
         <>
@@ -30,13 +31,30 @@ export function Home(){
             </div>
         </section>
 
-       
         <section className="container">
-            <section className="row-cols-1 row-cols-md-5 g-3">
+            <section className="row row-cols-1 row-cols-md-5 g-3 fondo2">
                 {
                     //logica para recorrer la BD y mostrar la informacion de los 10 barberos
-                }
-            </section>
+
+                    basedatos.map(function(barbero){
+                        return(
+                            <div className="col">
+                                <div className="card h-100 shadow mb-2 fondo">
+                                <h5>{barbero.nombre}:</h5>
+                                <h6>{barbero.especialidad}</h6>
+                                <h6>{barbero.experiencia}</h6>
+                                <h6>{barbero.correo}</h6>
+                                <h6>{barbero.contacto}</h6>
+                                           
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+
+                
+                </section>
+            
         </section>
 
 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "bootstrap/dist/js/bootstrap.min.js"
-import "bootstrap/dist/css/bootstrap.min.css"
-
-
-import { Home } from './Home/Home.jsx'
-
-
-
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  
-    <Home></Home>
-  
+    <App />
   </React.StrictMode>,
 )
